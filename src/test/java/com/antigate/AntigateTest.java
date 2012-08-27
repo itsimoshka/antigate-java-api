b01c0d5ef1584fdcca2afb1c80a8a7c5d63e9dc1
itsimoshka <itsimoshka@tut.by>
2012-08-27 19:21:39 +0300

    Added use AntigateConfig, result of sendFile and changed AntigateException catching

--- src/test/java/com/antigate/AntigateTest.java
+++ src/test/java/com/antigate/AntigateTest.java
@@ -1,6 +1,7 @@
 package com.antigate;
 
 
+import com.antigate.config.AntigateConfig;
 import com.antigate.execption.AntigateException;
 import org.junit.Test;
 
@@ -14,11 +15,12 @@ public class AntigateTest {
     @Test
     public void sendFileTest() {
         try {
-            Antigate.sendFile("1.png");
+            AntigateConfig.setKey("2170596c4b629bb7064a64a7b6f8b30d");
+            System.out.println(Antigate.sendFile("1.png"));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (AntigateException e) {
-            e.printStackTrace();
+            System.out.println(e.getErrorCode());
         }
     }
 }
