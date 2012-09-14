d1ae0a2e059b4c6223859d52dca5ee190d071bd8
itsimoshka <itsimoshka@tut.by>
2012-09-15 02:27:54 +0300

    Added URL const for getting captcha status

--- src/main/java/com/antigate/config/consts/AntigateConstants.java
+++ src/main/java/com/antigate/config/consts/AntigateConstants.java
@@ -4,7 +4,7 @@ package com.antigate.config.consts;
  * @author itsimoshka
  */
 public class AntigateConstants {
-    public static final String ANTIGATE_URL = "http://antigate.com/";
+    private static final String ANTIGATE_URL = "http://antigate.com/";
     public static final String SEND_FILE_CAPTCHA_URL = ANTIGATE_URL + "in.php";
     public static final String GET_CAPTCHA_STATUS_URL = ANTIGATE_URL + "res.php";
     public static final String OK_PREFIX = "OK|";
